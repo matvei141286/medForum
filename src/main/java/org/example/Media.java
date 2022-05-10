6d488a0f65d0fd78c960e2d0b9c6ece268113fb1
matvei141286 <matvei141286@mail.ru>
2022-05-10 22:04:18 +0300

    use Construct_Anotation_Lombok in Media.java

--- src/main/java/org/example/Media.java
+++ src/main/java/org/example/Media.java
@@ -1,30 +1,19 @@
 package org.example;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
 import java.util.*;
 
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode
+@RequiredArgsConstructor
 
 public class Media {
-    private String name;
-    private String fileExtension;
-    private String size;
+    private final  String name;
+    private final  String fileExtension;
+    private final String size;
     private String typeMedia;
-    private Date dateCreation;
-
-    public Media (String name, String fileExtension, String size) {
-        this.name = name;
-        this.fileExtension= fileExtension;
-        this.size = size;
-        this.dateCreation = new Date();
-
-    }
-
-
+    private Date dateCreation = new Date();
 }
